027920e716f6c9d01cfc2e06ed6661a2cd384116
Yogananda K S <152706925+Yogi2084@users.noreply.github.com>
2026-09-12 11:41:34 +0530

    Updated index.ts

--- lib/environment/index.ts
+++ lib/environment/index.ts
@@ -1,2 +1,4 @@
-export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || process.exit(1);
-export const clientUrl = process.env.NEXT_PUBLIC_CLIENT_URL || process.exit(1);
+export const serverUrl =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const clientUrl =
+  process.env.NEXT_PUBLIC_CLIENT_URL || "http://localhost:4000";
